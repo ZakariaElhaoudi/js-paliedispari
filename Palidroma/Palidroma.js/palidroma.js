350c627f5092070d5fc47e1713433c5cd91ed3d2
Zakaria96 <zakarialavoro@gmail.com>
2023-05-08 15:09:25 +0200

    struttura codice esercizio palidroma

---
+++ Palidroma/Palidroma.js/palidroma.js
@@ -0,0 +1,8 @@
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+// Chiedere all’utente di inserire una parola
+
+const parolaUser =prompt("inserisci una parola")
+
+console.log(parolaUser);
