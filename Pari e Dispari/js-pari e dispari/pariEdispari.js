9ab9d37e1ff56e253484fc6d287bbc4a25b8d038
Zakaria96 <zakarialavoro@gmail.com>
2023-05-08 16:48:06 +0200

    parte logica compito completata

--- Pari e Dispari/js-pari e dispari/pariEdispari.js
+++ Pari e Dispari/js-pari e dispari/pariEdispari.js
@@ -6,7 +6,7 @@ Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (u
 
 // L’utente sceglie pari o dispari 
 const sceltaUser=prompt("Scegli tra pari o dispari")
-
+console.log(sceltaUser);
 
 // inserisce un numero da 1 a 5.
 const numeroUser =parseInt(prompt("Insercisci un numero da 1 a 5!"))
@@ -15,19 +15,29 @@ console.log(numeroUser);
 // Generiamo un numero random per il computer usando una funzione
 function numRandomPc (min, max) {
 
-   const numeroPc = Math.floor(Math.random() * (max - min) ) + min;
+   const numeroPc = Math.floor(Math.random() * (max - min + 1) ) + min;
     
    return numeroPc;
 }
-console.log(numRandomPc(1, 5));
+const numeroPc = numRandomPc(1, 5);
+console.log(numeroPc);
 // Sommiamo i due numeri
- let somma = numeroUser + numRandomPc (1, 5);
+ let somma = numeroUser + numeroPc;
     console.log(somma);
 
 // Stabiliamo se la somma dei due numeri è pari o dispari 
-//    if (numero % 2 === 0 ){
-//     return  "pari";
-//    } else {
-//     return  "dispari";
-//    }
+function controlloPariDispari(numeroPariDispari) {
+    if (numeroPariDispari % 2 === 0 ){
+    return  "pari";
+   } else {
+    return  "dispari";
+   }
+}
+const risultatoPariDispari = controlloPariDispari(somma);
+console.log(risultatoPariDispari);
 // Dichiariamo chi ha vinto.
+if (risultatoPariDispari === sceltaUser) {
+    console.log("you win !!!");
+} else {
+    console.log("you lose !!!");
+}
